6cf23dfadeb6cf8bafdb5d251a9f5caf1b95cc37
mandiraadak <30654150+mandiraadak@users.noreply.github.com>
2017-08-30 00:59:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,6 @@ var ele = document.getElementById("hi");
 ele.innerHTML = "New val";
 
 var img = document.getElementById("im");
-img.onclick = function(){
+img.onclick = function () {
     img.style.marginleft = "100px";
 };
