4c7e949bc32aa1e5a90edcc6b79bdb710d83710e
mandiraadak <30654150+mandiraadak@users.noreply.github.com>
2017-08-30 12:51:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,5 +13,5 @@ button.onclick = function () {
         }
     };
     request.open('GET', 'http://mandirama15e.imad.hasura-app.io/counter',true);
-    request.sent(null);
+    request.send(null);
 }
