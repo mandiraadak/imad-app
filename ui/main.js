22bf7a4f25c95c804ddcbb27b5f3b31f5be5f1f8
mandiraadak <30654150+mandiraadak@users.noreply.github.com>
2017-08-30 13:31:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,6 +7,8 @@ button.onclick = function () {
         if(request.readyState === XMLHttpRequest.DONE){
             if(request.status === 200){
                var names = ['Name1', 'Name2', 'Name3'];
+               var names = request.responseText;
+               names = JSON.parse()
     var list = '';
     for(var i=0; i < names.length; i++){
         list += '<li>' +names[i] + '</li>';
