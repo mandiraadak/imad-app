0d4ce29d80841746816218fdf73ee4a5c301b867
mandiraadak <30654150+mandiraadak@users.noreply.github.com>
2017-08-30 13:06:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,6 +26,6 @@ submit.onclick = function () {
     for(var i=0; i < names.length; i++){
         list += '<li>' +names[i] + '</li>';
     }
-    var ul = documnet.getElementById('namelist');
+    var ul = document.getElementById('namelist');
     ul.innerHTML;
 }
